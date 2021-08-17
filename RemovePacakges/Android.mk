a50b0d4b5ab075324f4cd13971070ec3f77465f3
Ellen <ellenaosp@protonmail.com>
2021-08-17 16:43:57 +0000

    Dont build Velvet

--- RemovePacakges/Android.mk
+++ RemovePacakges/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Gallery2 Jelly Email PhotoTable Photos Chrome Recorder Eleven ViaBrowser Music DuckDuckGo SimpleGallery Phonograph Maps Gmail2 Tycho GCS 
+LOCAL_OVERRIDES_PACKAGES := Gallery2 Jelly Email PhotoTable Photos Chrome Recorder Eleven ViaBrowser Music DuckDuckGo SimpleGallery Phonograph Maps Gmail2 Tycho GCS Velvet
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
